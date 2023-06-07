a68b872087138e20efb96ee067e259e76ad3f944
vijaykumarsi <122625279+vijaykumarsi@users.noreply.github.com>
2023-06-07 13:27:44 +0530

    Add files via upload
    updated

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Form from './components/Form';
+
+
+
+
+function App() {
+  return (
+   <div>
+    <h1>EMPLOYEE FEEDBACK FORM</h1>
+    <Form />
+
+   
+   
+
+   </div> 
+  );
+}
+
+export default App;
